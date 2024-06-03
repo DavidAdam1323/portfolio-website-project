175198f2a68108a543ed51eab86b18f1e1ee9777
DavidAdam1323 <davidadamgtr@gmail.com>
2024-06-04 00:22:32 +0100

    Enhance layout and styling for skills and technology lists with responsive design

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,33 +13,43 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h3>Explore My</h3>
       <h1 className="sectionTitle">Experience</h1>
-      <div className={styles.frontEndList}>
+      <div className={styles.containerList}>
         <div className={styles.skillsList}>
-          <h3>Frontend Development</h3>
-          <SkillsList src={checkMarkIcon} skill="HTML" />
-          <SkillsList src={checkMarkIcon} skill="CSS" />
-          <SkillsList src={checkMarkIcon} skill="JavaScript" />
-          <SkillsList src={checkMarkIcon} skill="TypeScript" />
-          <SkillsList src={checkMarkIcon} skill="React" />
-          <SkillsList src={checkMarkIcon} skill="Figma" />
-          <SkillsList src={checkMarkIcon} skill="UX|UI" />
-          <SkillsList src={checkMarkIcon} skill="Tailwind CSS" />
-          <SkillsList src={checkMarkIcon} skill="Git & GitHub" />
+          <h3>
+            Frontend <br />
+            Development
+          </h3>
+          <div className={styles.techList}>
+            <SkillsList src={checkMarkIcon} skill="HTML" />
+            <SkillsList src={checkMarkIcon} skill="CSS" />
+            <SkillsList src={checkMarkIcon} skill="JavaScript" />
+            <SkillsList src={checkMarkIcon} skill="TypeScript" />
+            <SkillsList src={checkMarkIcon} skill="React" />
+            <SkillsList src={checkMarkIcon} skill="Figma" />
+            <SkillsList src={checkMarkIcon} skill="UX|UI" />
+            <SkillsList src={checkMarkIcon} skill="Tailwind CSS" />
+            <SkillsList src={checkMarkIcon} skill="Git & GitHub" />
+          </div>
         </div>
       </div>
 
-      <div className={styles.backEndList}>
+      <div className={styles.containerList}>
         <div className={styles.skillsList}>
-          <h3>Backend Development</h3>
-          <SkillsList src={checkMarkIcon} skill="Node JS" />
-          <SkillsList src={checkMarkIcon} skill="Express JS" />
-          <SkillsList src={checkMarkIcon} skill="SQL" />
-          <SkillsList src={checkMarkIcon} skill="Postgres" />
-          <SkillsList src={checkMarkIcon} skill="Python" />
-          <SkillsList src={checkMarkIcon} skill="TensorFlow" />
-          <SkillsList src={checkMarkIcon} skill="Playwright" />
-          <SkillsList src={checkMarkIcon} skill="Docker" />
-          <SkillsList src={checkMarkIcon} skill="GitHub Actions" />
+          <h3>
+            Backend <br />
+            Development
+          </h3>
+          <div className={styles.techList}>
+            <SkillsList src={checkMarkIcon} skill="Node JS" />
+            <SkillsList src={checkMarkIcon} skill="Express JS" />
+            <SkillsList src={checkMarkIcon} skill="SQL" />
+            <SkillsList src={checkMarkIcon} skill="Postgres" />
+            <SkillsList src={checkMarkIcon} skill="Python" />
+            <SkillsList src={checkMarkIcon} skill="TensorFlow" />
+            <SkillsList src={checkMarkIcon} skill="Playwright" />
+            <SkillsList src={checkMarkIcon} skill="Docker" />
+            <SkillsList src={checkMarkIcon} skill="GitHub Actions" />
+          </div>
         </div>
       </div>
     </section>
