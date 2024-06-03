aeb36b5239269bd59fb9d10888f84247f6d1f170
DavidAdam1323 <davidadamgtr@gmail.com>
2024-06-03 04:27:03 +0100

    Create Contact component with basic structure for contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Skills from "./sections/Skills/Skills";
 import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactStyles.module.css";
+
+function Contact() {
+  return (
+    <section id="contact" className={styles.container}>
+      <h3 className={styles.subTitle}>Get In Touch</h3>
+      <h1 className="sectionTitle">Contact</h1>
+    </section>
+  );
+}
+
+export default Contact;
