f6c4896dd5be171898136ab9f7b1cf54914dc078
DavidAdam1323 <davidadamgtr@gmail.com>
2024-06-03 01:44:39 +0100

    added Skills component to the project and implemented a Skills section with Expirience heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
+import Skills from "./sections/Skills/Skills";
 import Projects from "./sections/Projects/Projects";
 
 function App() {
   return (
     <>
       <Hero />
+      <Skills />
       <Projects />
     </>
   );
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import styles from "./SkillsStyles.module.css";
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h3>Explore My</h3>
+      <h1>Experience</h1>
+    </section>
+  );
+}
+
+export default Skills;
